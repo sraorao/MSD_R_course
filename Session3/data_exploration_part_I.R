6d1092d1413c878ea6afa920da7822639a05c1ef
Srinivasa Rao <srinivasa.rao@nds.ox.ac.uk>
2021-01-26 21:23:59 +0000

    arrange, mutate, if_else

--- Session3/data_exploration_part_I.R
+++ Session3/data_exploration_part_I.R
@@ -22,13 +22,21 @@ View(owid_covid)
 str(owid_covid)
 
 owid_covid$location
+unique(owid_covid$location)
+
 # DATA TYPES - do all the columns look okay, does anything need to be changed?
 ?Dates
 owid_covid$date = as.Date(owid_covid$date)
 new_year = "2021-01-01"
 
 # MISSING DATA - are there any missing data? Do you want to remove/replace them? ####
+# No for this dataset, because it's a very clean one already
+# Can NAs be substituted with zero, for example in the total_vaccinations column?
 
+# SORT data ####
+
+owid_covid %>%
+  arrange(date) -> owid_covid_sorted_by_date
 
 # SUBSET DATA - which columns should we keep? ####
 
@@ -36,8 +44,9 @@ names(owid_covid)
 
 owid_covid %>%
   select() -> owid_covid_selected
-
+head(owid_covid_selected)
 # FILTER DATA - should we filter data? What criteria should we use? ####
+# The answer will depend on what you want to get out of the data
 
 owid_covid %>%
   filter(location == "United Kingdom") -> owid_covid_uk
@@ -46,21 +55,25 @@ owid_covid %>%
   filter(location == "Belgium") -> owid_covid_belgium
 
 plot(x = owid_covid_uk$date, y = owid_covid_uk$total_cases_per_million)
+plot(x = owid_covid_uk$date, y = owid_covid_uk$total_cases_per_million, col = "red")
 plot(x = owid_covid_uk$date, y = owid_covid_uk$total_cases_per_million, type = "l")
 
-# Q: Plot the total deaths in the US across time
+# Q: Plot total deaths in the US across time
 
 
-# Exploring - TO DELETE
+# CREATE new columns based on existing data ####
 
 owid_covid %>%
-  mutate(hdi_class = if_else(human_development_index < 0.5, "low", if_else(human_development_index > 0.9, "high", "moderate"))) %>%
-  select(date, population, population_density, total_cases_per_million, total_deaths, location, hdi_class) %>%
-  filter(hdi_class != "moderate") %>%
-  ggplot(aes(x = date, y = total_cases_per_million, group = location, colour = hdi_class)) +
-    geom_line() +
-    theme_bw()
+  mutate(prop_new_deaths_to_cases = new_deaths/new_cases) -> owid_covid_prop_deaths_cases
+head(owid_covid_prop_deaths_cases)
+
+# create a new column using conditionals: using the if_else() function
+owid_covid %>%
+  mutate(hdi_class = if_else(human_development_index < 0.7, "red","blue")) -> owid_covid_hdi_class
+
+plot(x = owid_covid_hdi_class$date, y = owid_covid_hdi_class$total_deaths_per_million, col = owid_covid_hdi_class$hdi_class)
 
 # PROBLEM SET ####
+
 
 
