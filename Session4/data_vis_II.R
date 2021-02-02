c13687de0aad32b6a007b1c4cbc89f4c09c50025
Irina Chelysheva <irina.chelysheva@paediatrics.ox.ac.uk>
2021-02-02 19:49:44 +0000

    Updated version from today's session

--- Session4/data_vis_II.R
+++ Session4/data_vis_II.R
@@ -1,6 +1,6 @@
 ###R for biologists
 ##Irina & Rao, 02/02/2021
-
+library(tidyverse)
 #Import the all sheets from Excel file provided (use readxl)
 library(readxl)
 ifny <- read_excel("Session4/data/MSD_data.xlsx", sheet = 'IFNy')
@@ -20,50 +20,68 @@ il10<-il10[,c(1,5,6)]
 
 cyto <- Reduce(function(...) merge(..., all = TRUE, by = c("Participant_ID","Randomised_to")),
        list(ifny, il1, il2, il10))
-colnames(cyto)<-c("ID","Randomisation","IFNy","IL-1","IL-2","IL-10")
+colnames(cyto)<-c("ID","Randomisation","IFNy","IL1","IL2","IL10")
 #Remove negative values
+cyto %>% filter(across(c(IFNy, IL1, IL2, IL10),~ .x>0|is.na(.x)==TRUE)) -> cyto
 cyto %>% filter(across(c(-ID,-Randomisation),~ .x>0|is.na(.x)==TRUE)) -> cyto
 
 #Convert groups into factors
 cyto$Randomisation <- as.factor(cyto$Randomisation)
 levels(cyto$Randomisation)
+c(1,2)
 levels(cyto$Randomisation)<-c("ChAdOx1", "Control")
+#install.packages("data.table")
 library(data.table)
 #Wide to long format
-cyto_melted<-melt(cyto,id.vars=c("ID","Randomisation"))
-cyto_melted<-na.omit(cyto_melted)
 
+#cyto_melted<-melt(cyto,id.vars=c("ID","Randomisation"))
+#reshape(cyto,idvar = c("ID","Randomisation"),direction = "long")
+
+#Stick with gather from tidyr
+cyto_melted<-gather(cyto,key="variable",value = "value",-ID,-Randomisation)
+cyto_melted<-na.omit(cyto_melted)
+#cyto2<-na.omit(cyto)
+#library(reshape2)
+
+library(ggplot2)
 #Barplot
-ggplot(cyto_melted, aes(fill=Randomisation, y=(value), x=variable)) + 
+ggplot(cyto_melted, aes(fill=Randomisation, y=value, x=variable)) + 
   geom_bar(position="dodge", stat="identity") +
+  theme_bw()
+
+#Compare to this - we plot the means
+ggplot(cyto_melted, aes(fill=Randomisation, y=value, x=variable)) + 
+  geom_bar(position="dodge", stat = "summary", fun = "median") +
   theme_bw()
 
 #Stacked barplot
 ggplot(cyto_melted, aes(fill=Randomisation, y=value, x=variable)) + 
-  geom_bar(position="stack", stat="identity") +
+  geom_bar(position="stack", stat = "summary",) +
   theme_bw()
 
 #Percent stacked barplot
 ggplot(cyto_melted, aes(fill=Randomisation, y=value, x=variable)) + 
-  geom_bar(position="fill", stat="identity") +
+  geom_bar(position="fill", stat="summary", fun = "mean") +
   theme_bw()
 
 #Reverse the groupping - check the relative aboundance of each cytokine by randomisation group
 ggplot(cyto_melted, aes(fill=variable, y=value, x=Randomisation)) + 
-  geom_bar(position="fill", stat="identity",width = 0.5) +
+  geom_bar(position="fill", stat="identity", width = 0.5) +
   theme_bw()
 
 #Add error bars
 ggplot(cyto_melted, aes(fill=Randomisation, y=(value), x=variable)) + 
-  geom_bar(position="dodge", stat="identity") + 
-  stat_summary(geom = "errorbar", fun.data = mean_se, position="dodge") +
+  geom_bar(position="dodge", stat = "summary", fun = "mean") + 
+  stat_summary(geom = "errorbar", fun.data = mean_se, 
+               position = position_dodge(width = 0.90), width = 0.5) +
   theme_bw()
 
 #Bar plots is not a good way to represent our data!
 
 #Boxplot
 ggplot(cyto_melted, aes(fill=Randomisation, y=value, x=variable)) + 
-  geom_boxplot()
+  geom_boxplot() +
+  theme_bw()
 
 #Scale - log transform
 ggplot(cyto_melted, aes(fill=Randomisation, y=log(value), x=variable)) + 
@@ -81,8 +99,9 @@ ggplot(cyto_melted, aes(fill=Randomisation, y=log(value), x=variable)) +
   theme_bw() +
   theme(legend.position = "none")+
   facet_wrap(~Randomisation)
-
-#Dotblot + means
+##########################
+#####End of session 4#####
+#Dotplot + means
 ggplot(cyto_melted, aes(fill=Randomisation, y=log(value), x=variable))+
   geom_dotplot(binaxis = "y", stackdir = "center",
                position = position_dodge(0.8), dotsize = 0.8) + 
